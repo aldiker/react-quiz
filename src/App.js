03c2bfdcac8bb55b0775b9857212f15d0b64e3c5
Oleksandr Polivanov <aldiker@ukr.net>
2024-03-07 13:28:21 +0200

    Added StartScreen component and implemented it.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import { useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
+import Loader from './components/Loader'
+import Error from './components/Error'
+import StartScreen from './components/StartScreen'
+
+const initialState = {
+    questions: [],
+    // 'loading', 'error', 'ready', 'active', 'finished'
+    status: 'loading',
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived':
+            return { ...state, status: 'ready', questions: action.payload }
+        case 'dataFailed':
+            return { ...state, status: 'error' }
+        default:
+            throw new Error('Action unknown')
+    }
+}
 
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    const { questions, status } = state
+    const numQuestions = questions.length
+
     useEffect(() => {
         fetch('http://localhost:8000/questions')
             .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch((err) => console.log('Error'))
+            .then((data) => {
+                console.log(data)
+                dispatch({ type: 'dataReceived', payload: data })
+            })
+            .catch((err) => {
+                console.log('Error')
+                dispatch({ type: 'dataFailed' })
+            })
     }, [])
 
     return (
         <div className="app">
             <Header />
             <Main>
-                <p>1/15</p>
-                <p>Question?</p>
+                {status === 'loading' && <Loader />}
+                {status === 'error' && <Error />}
+                {status === 'ready' && (
+                    <StartScreen numQuestions={numQuestions} />
+                )}
             </Main>
         </div>
     )
