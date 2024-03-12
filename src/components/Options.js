f109878bb366e7f71458cecac43b738bb320d20e
Oleksandr Polivanov <aldiker@ukr.net>
2024-03-12 14:48:31 +0200

    Added states "answer" and "points"

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,28 @@
-export default function Options({ question }) {
-	return (
-		<div className="options">
-			{question.options.map((option, idx) => (
-				<button className="btn btn-option" key={idx}>
-					{option}
-				</button>
-			))}
-		</div>
-	)
+export default function Options({ question, dispatch, answer }) {
+    const hasAnswer = answer !== null
+
+    return (
+        <div className="options">
+            {question.options.map((option, idx) => (
+                <button
+                    className={`btn btn-option ${
+                        idx === answer ? 'answer' : ''
+                    } ${
+                        hasAnswer
+                            ? idx === question.correctOption
+                                ? 'correct'
+                                : 'wrong'
+                            : ''
+                    }`}
+                    key={idx}
+                    disabled={hasAnswer}
+                    onClick={() =>
+                        dispatch({ type: 'newAnswer', payload: idx })
+                    }
+                >
+                    {option}
+                </button>
+            ))}
+        </div>
+    )
 }
